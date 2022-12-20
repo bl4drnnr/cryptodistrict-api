0d0adc420dce8f00b7d55323e27854479ef1694a
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-12-20 21:11:36 +0100

    prisma config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
+import { PrismaModule } from './modules/prisma/prisma.module';
+import { SharedModule } from './shared/shared.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    AuthModule,
+    UserModule,
+    PrismaModule,
+    SharedModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`
+    })
+  ]
 })
 export class AppModule {}
