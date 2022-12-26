654a5600652b59568e9b95945495d08b11fda607
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-12-26 11:11:32 +0100

    basic auth middleware

--- src/middlewares/basic-auth.middleware.ts
+++ src/middlewares/basic-auth.middleware.ts
@@ -1,32 +1,31 @@
 import type { NestMiddleware } from '@nestjs/common';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import type { NextFunction, Request, Response } from 'express';
-import passport from 'passport';
-import { BasicStrategy } from 'passport-http';
 
 import { ApiConfigService } from '@shared/services/config.service';
 
 @Injectable()
 export class BasicAuthMiddleware implements NestMiddleware {
-  constructor(private configService: ApiConfigService) {
-    passport.use(
-      new BasicStrategy(function (username, password, done) {
-        if (
-          configService.basicAuthConfig.username === username &&
-          configService.basicAuthConfig.password === password
-        ) {
-          return done(null, true);
-        }
-
-        return done(new UnauthorizedException());
-      })
-    );
-  }
+  constructor(private configService: ApiConfigService) {}
 
   use(req: Request, res: Response, next: NextFunction) {
-    const authenticate = () =>
-      passport.authenticate('basic', { session: false })(req, res, next);
+    if (req.headers['authorization']) {
+      const authorization = req.headers['authorization'];
+      const basic = authorization.match(/^Basic (.+)$/);
 
-    return authenticate();
+      if (!basic) return new UnauthorizedException();
+
+      const credentials = Buffer.from(basic[1], 'base64').toString('utf-8');
+
+      const apiUsername = this.configService.basicAuthConfig.username;
+      const apiPassword = this.configService.basicAuthConfig.password;
+
+      if (credentials != `${apiUsername}:${apiPassword}`) {
+        return new UnauthorizedException();
+      }
+
+      return next();
+    }
+    return new UnauthorizedException();
   }
 }
